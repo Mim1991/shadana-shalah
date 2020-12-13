f00a7d6a1fda824d086df0eb488d37124224986b
Meehir Patel <meehir_patel@hotmail.co.uk>
2020-12-13 21:13:33 +0000

    controlllers created

--- app/models/yoga_class.rb
+++ app/models/yoga_class.rb
@@ -1,2 +1,4 @@
 class YogaClass < ApplicationRecord
+  has_many :bookings
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
+
+  resources :yoga_class do
+    resources :bookings, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :bookings, only:[:index, :show, :destroy] do
+    member do
+      get 'confirmation'
+    end
+  end
+
+  get 'profile', action: :show, controller: 'users'
+
 end
