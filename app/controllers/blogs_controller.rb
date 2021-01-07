68cd8a2e5474b923842b4069036505cc33d5543d
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-07 17:28:09 +0000

    added new blog and create methods

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,30 @@
 class BlogsController < ApplicationController
-  
+
   def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content, :category)
   end
 end
